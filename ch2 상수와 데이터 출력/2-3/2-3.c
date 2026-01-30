0bb7359c31c97601119db4fee7ad1535be239f86
BLUEBERRY <dlatmdwnsdla@gmail.com>
2026-01-30 22:48:30 +0900

    초기 커밋.

---
+++ ch2 상수와 데이터 출력/2-3/2-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Be happy\n");
+	printf("12345678901234567890\n");
+	printf("My\tfriend\n");
+	printf("Goot\bd\tchance\n");	//\b 왼쪽으로 한칸 d를 t로 바꿈 \t 탭 위치로 이동
+	printf("Cow\rw\a\n");	//\r 맨앞으로 이동해서 C를 r로 바꿈 \a 벨소리를 냄
+
+	return 0;
+}
